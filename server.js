f64d6a36197e0dd00962320002dafbf77e6816fb
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-01-26 15:11:38 +0000

    Version update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.listen(port, () => {
   var os = require("os");
   
   console.log('------------------------------------');
-  console.log('Development version: 1.3.0');
+  console.log('Development version: 1.3.1');
   console.log('Running on host: '+os.hostname());
   console.log(`Available at port: ${port}`);
   console.log('------------------------------------');
