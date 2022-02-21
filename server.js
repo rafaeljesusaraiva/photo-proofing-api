f79c59c68c4feb745e4235c9c5bdf1a0edc9600a
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-02-21 01:31:03 +0000

    cors allow any

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const app = require('./config/express')();
-const port = app.get('port');
-require('dotenv').config();
+const app = require("./config/express")();
+const port = app.get("port");
+require("dotenv").config();
 
 // RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(port, () => {
   global.__basedir = __dirname;
 
   var os = require("os");
-  
-  console.log('------------------------------------');
-  console.log('Development version: 1.3.5');
-  console.log('Running on host: '+os.hostname());
+
+  console.log("------------------------------------");
+  console.log("Development version: 1.3.6");
+  console.log("Running on host: " + os.hostname());
   console.log(`Available at port: ${port}`);
-  console.log('------------------------------------');
+  console.log("------------------------------------");
 });
